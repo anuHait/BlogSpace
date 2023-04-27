fa4376a5a0e55b41024a29f6b75b1bef41883c22
anuHait <anusmitahait67@gmail.com>
2023-04-27 17:54:57 +0530

    chota updTES

--- blogyy/src/App.js
+++ blogyy/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './component/Login';
+import CreatePost from './component/createPost';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <CreatePost/>
     </div>
   );
 }
--- blogyy/src/component/createPost.js
+++ blogyy/src/component/createPost.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function CreatePost() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div>
-      <form action="">
-      
-      </form>
-    </div>
+    <>
+    <button onClick={handleShow}>
+    hi i am modal 
+    </button>
+
+      <Modal
+        show={show}
+        size="lg"
+        onHide={handleClose}
+        
+        keyboard={false}
+        
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <label htmlFor="mane" className='text-cyan-600'>Name</label>
+          <input type="text" className='border-cyan-800' />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    
+    </>
+    
   )
 }
 
