a92e9522c0f97c2a4495323270815f8bc8bcd578
anuHait <anusmitahait67@gmail.com>
2023-05-07 19:22:00 +0530

    hi

--- blogyy/src/component/hello.js
+++ blogyy/src/component/hello.js
@@ -5,10 +5,10 @@ function Hello() {
     const [tab, setTab] = useState("");
     const[posts,setPosts]=useState(null);
     const handleAllClick = () => setTab("");
-    const handleTechClick = () => setTab("meow2");
-    const handleSportsClick = () => setTab("meow1");
-    const handleFashClick = () => setTab("fash");
-    const handleTravClick = () => setTab("trav");
+    const handleTechClick = () => setTab("Technology");
+    const handleSportsClick = () => setTab("Sports");
+    const handleFashClick = () => setTab("fashion");
+    const handleTravClick = () => setTab("travel");
 
     useEffect(()=>{
         axios.get(`http://localhost:4000/getPosts/${tab}`)
@@ -19,7 +19,7 @@ function Hello() {
         .catch((err)=>{console.log(err)})
     },[tab])
     return (
-        <div className='flex flex-row justify-center items-center'>
+        <div className='flex flex-col justify-center items-center'>
             <div className='flex flex-row gap-3'>
                 <button onClick={handleAllClick} className='text-xl font-semibold text-gray-600 focus:bg-blue-500 focus:text-white p-1 rounded-2xl w-[3vw]'>All</button>
                 <button onClick={handleTechClick} className='text-xl font-semibold text-gray-600 focus:bg-blue-500 focus:text-white p-1 rounded-2xl w-[8vw]'>Technology</button>
@@ -27,13 +27,13 @@ function Hello() {
                 <button onClick={handleFashClick} className='text-xl font-semibold text-gray-600 focus:bg-blue-500 focus:text-white p-1 rounded-2xl w-[7vw]'>Fashion</button>
                 <button onClick={handleTravClick} className='text-xl font-semibold text-gray-600 focus:bg-blue-500 focus:text-white p-1 rounded-2xl w-[6vw]'>Travel</button>
             </div>
-            <div className='flex flex-row flex-wrap gap-3'>
+            <div className='flex flex-row flex-wrap gap-3 justify-center items-center'>
         {   posts?.map((post)=>(
             <BlogBox 
             image={post?.image} 
             cate={post?.category} 
             title={post?.title} 
-            desc={post?.desc}/>
+            desc={post?.description}/>
         ))         
             
 
--- blogyy/src/component/navbar.js
+++ blogyy/src/component/navbar.js
@@ -7,10 +7,10 @@ function Navbar() {
   
   const user = cookies.get("imgurl");
   return (
-    <div className='flex flex-row justify-between '>
-      <div></div>
+    <div className='flex flex-row justify-between m-6'>
+      <div><h1 className='text-4xl font-bold text-[#7a49c9]'>Webimple</h1></div>
       <div >
-      <ul className='flex flex-row gap-3 m-6'>
+      <ul className='flex flex-row gap-3 '>
       <li className='text-2xl font-semibold text-[#AA77FF] p-2'>Home</li>
       <li><CreatePost/></li>
       <div >
